c504dcf2c210a0a79f662f35e4c31c93e7871a9b
mayanksomani07 <83702471+mayanksomani7@users.noreply.github.com>
2023-05-10 09:59:32 +0530

    initial commit

---
+++ nft_card_game/client/src/contract/index.js
@@ -0,0 +1,4 @@
+import contract from './AVAXGods.json';
+
+export const ADDRESS = '0x7cF8F1Db5edc4757e42F15b8B0912C520F0f9C6B';
+export const { abi: ABI } = contract;
---
+++ nft_card_game/client/src/page/index.js
@@ -0,0 +1,13 @@
+import Home from './Home';
+import Battle from './Battle';
+import BattleGround from './BattleGround';
+import CreateBattle from './CreateBattle';
+import JoinBattle from './JoinBattle';
+
+export {
+  Battle,
+  Home,
+  BattleGround,
+  CreateBattle,
+  JoinBattle
+};
